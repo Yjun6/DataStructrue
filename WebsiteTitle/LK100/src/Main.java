e30ee07a4de4f95a0911b44232d406b8f2687b94
yjun6 <Su_Xi_03@outlook.com>
2023-04-21 19:52:59 +0800

    力扣100

---
+++ WebsiteTitle/LK100/src/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:${USER}
+ * Date:${YEAR}-${MONTH}-${DAY}
+ * Time:${TIME}
+ */
+//题解
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
