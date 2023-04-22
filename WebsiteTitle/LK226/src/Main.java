03aeb1edf4f9f8b89817eb7c0ddbc83e815797f5
yjun6 <Su_Xi_03@outlook.com>
2023-04-22 19:20:28 +0800

    力扣226

---
+++ WebsiteTitle/LK226/src/Main.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:${USER}
+ * Date:${YEAR}-${MONTH}-${DAY}
+ * Time:${TIME}
+ */
+//
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
